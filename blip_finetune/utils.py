978779b42b1537fac32ac934e8f238086c3c2d50
Anouk <97145199+anoukkim@users.noreply.github.com>
2024-12-20 02:26:57 +0900

    Update utils.py

--- blip_finetune/utils.py
+++ blip_finetune/utils.py
@@ -6,19 +6,13 @@ def load_config(file_path="config.json"):
         return json.load(file)
 
 def collate_fn(batch):
-    input_ids = torch.stack([torch.tensor(item['input_ids']) for item in batch])
-    attention_mask = torch.stack([torch.tensor(item['attention_mask']) for item in batch])
-    labels = torch.stack([torch.tensor(item['labels']) for item in batch])
-    pixel_values = torch.stack([torch.tensor(item['pixel_values']) for item in batch])
-
-    input_ids = input_ids.squeeze(1)
-    attention_mask = attention_mask.squeeze(1)
-    labels = labels.squeeze(1)
-    pixel_values = pixel_values.squeeze(1)
-
+    input_ids = torch.stack([torch.tensor(item["input_ids"]) for item in batch])
+    attention_mask = torch.stack([torch.tensor(item["attention_mask"]) for item in batch])
+    labels = torch.stack([torch.tensor(item["labels"]) for item in batch])
+    pixel_values = torch.stack([torch.tensor(item["pixel_values"]) for item in batch])
     return {
-        'input_ids': input_ids,
-        'attention_mask': attention_mask,
-        'labels': labels,
-        'pixel_values': pixel_values,
+        "input_ids": input_ids.squeeze(1),
+        "attention_mask": attention_mask.squeeze(1),
+        "labels": labels.squeeze(1),
+        "pixel_values": pixel_values.squeeze(1),
     }
