eb1691766e718a65494fe4933439eb73350b648e
Anouk <97145199+anoukkim@users.noreply.github.com>
2024-12-20 02:26:40 +0900

    Update train.py

--- blip_finetune/train.py
+++ blip_finetune/train.py
@@ -1,9 +1,7 @@
-from transformers import Trainer
+from transformers import Trainer, TrainingArguments
 from utils import collate_fn
 
 def train_model(model, processor, train_dataset, val_dataset, config):
-    from transformers import TrainingArguments
-
     training_args = TrainingArguments(
         output_dir=config["output_dir"],
         per_device_train_batch_size=config["train_batch_size"],
@@ -12,14 +10,12 @@ def train_model(model, processor, train_dataset, val_dataset, config):
         logging_dir=config["logging_dir"],
         num_train_epochs=config["num_train_epochs"],
         save_strategy="epoch",
-        logging_strategy="steps",
         logging_steps=config["evaluation_steps"],
         save_total_limit=config["save_total_limit"],
         load_best_model_at_end=True,
         learning_rate=config["learning_rate"],
         lr_scheduler_type=config["lr_scheduler_type"],
     )
-
     trainer = Trainer(
         model=model,
         args=training_args,
